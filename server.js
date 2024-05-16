adf59e183a368b7dcb67c766cd8c2125c02dff81
V1kata <viktor.burboran@gmail.com>
2024-05-16 18:23:57 +0300

    Creating and showing data with supabase

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const app = express();
-const sql = require('mssql');
+const { createClient } = require('@supabase/supabase-js');
+
+const supabase = createClient('https://jsqfijyqkatobeptpuwo.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzcWZpanlxa2F0b2JlcHRwdXdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU4NzEyMzAsImV4cCI6MjAzMTQ0NzIzMH0.ADaf9peWyi_aSgkPtjfwye4S0QnvcWHxI7VUz0GCgJw')
 
 app.get('/', async (req, res) => {
-    res.write('<p style="color: red;">Working</p>');
-})
+    const { data, error } = await supabase.from('UserData').select('*');
+    console.log(data)
+
+    // let postData = {
+    //     username: 'V1kata',
+    //     avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMpIZbN3HsqSADBtsTrCB_An3KE-D3IIQknxdmXsR9AQ&s',
+    //     skills: JSON.stringify(['driving', 'frontend', 'react']),
+    //     projects: JSON.stringify(['sudoku', 'moms-site']),
+    //     music: JSON.stringify(['alive-neffex', 'idk-here']),
+    //     links: JSON.stringify(['https://github.com', 'https://youtube.com']),
+    // }
+
+    // const { error } = await supabase.from('UserData').insert(postData);
+
+    // if (error) {
+    //     console.log(error)
+    // }
+
+    res.write(`<p style="color: red;">Working + ${data}</p>`);
+});
 
 app.listen(3000, () => console.log('Server is running http://localhost:3000'));
